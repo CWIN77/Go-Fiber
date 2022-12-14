81cf32e3edf8b689a5bfdb6a99173cde2e209ada
CWIN77 <cwstbp0426@gmail.com>
2022-12-14 14:03:46 +0900

    Tools 파일로 코드를 나눔

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
 	"log"
 	"os"
 
@@ -11,78 +8,27 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/compress"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+
+	mongodb "src/fiber/Tools"
 )
-
-func getData(client *mongo.Client) []byte {
-	coll := client.Database("simpleMongo").Collection("Post")
-	title := "Join the MongoDB Community"
-	var result bson.M
-	err := coll.FindOne(context.TODO(), bson.D{{Key: "title", Value: title}}).Decode(&result)
-	if err == mongo.ErrNoDocuments {
-		fmt.Printf("No document was found with the title %s\n", title)
-		return []byte("NoData")
-	}
-	if err != nil {
-		panic(err)
-	}
-	jsonData, err := json.MarshalIndent(result, "", "    ")
-	if err != nil {
-		panic(err)
-	}
-	return jsonData
-}
-
-type Request struct {
-	Id        string `json:"_id"`
-	AuthorId  string `json:"authorId"`
-	Content   string `json:"content"`
-	CreatedAt string `json:"createdAt"`
-	Title     string `json:"title"`
-	UpdatedAt string `json:"updatedAt"`
-}
-
-type Data struct {
-	Data Request `json:"data"`
-}
 
 func main() {
 	app := fiber.New()
-	envLoad()
-	uri := os.Getenv("MONGODB_URI")
-	if uri == "" {
-		log.Fatal("You must set your 'MONGODB_URI' environmental variable. See\n\t https://www.mongodb.com/docs/drivers/go/current/usage-examples/#environment-variable")
-	}
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
-	if err != nil {
-		panic(err)
-	}
-	defer func() {
-		if err := client.Disconnect(context.TODO()); err != nil {
-			panic(err)
-		}
-	}()
-	middleware(app)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		data := Request{}
-		json.Unmarshal(getData(client), &data)
-		db := Data{Data: data}
-		return c.Status(200).JSON(db)
-	})
-
-	app.Listen(":5000") // Elastic Beanstalk에 배포시 5000포트이고 파일이름이 application.go여야한다
-}
-
-func envLoad() {
+	// env 연결
 	if err := godotenv.Load(".env"); err != nil {
 		log.Fatal("Error loading .env file")
 	}
-}
 
-func middleware(app *fiber.App) {
+	client := mongodb.Connect(os.Getenv("MONGODB_URI"))
+
 	app.Use(cors.New())
 	app.Use(compress.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		data := mongodb.GetData(client)
+		return c.Status(200).JSON(data)
+	})
+
+	app.Listen(":5000") // Elastic Beanstalk에 배포시 5000포트이고 파일이름이 application.go여야한다
 }
