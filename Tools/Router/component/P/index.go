61638549f5d2c8ee85928d96e36f3c2876e00d60
CWIN77 <cwstbp0426@gmail.com>
2022-12-23 21:53:16 +0900

    component 데이터를 가져오는 함수 작성

--- Tools/Router/component/P/index.go
+++ Tools/Router/component/P/index.go
@@ -1,9 +1,42 @@
 package _component_P
 
 import (
+	"context"
+	"fiber/Tools/mongodb"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var Get = func(c *fiber.Ctx) error {
-	return c.SendString(c.Params("params"))
+	data, err := getData(mongodb.GetMongoClient())
+	if err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+	return c.Status(200).JSON(data)
+}
+
+func getData(client *mongo.Client) ([]map[string]interface{}, error) {
+	coll := client.Database("hvData").Collection("component")
+	opts := options.Find().SetSort(bson.D{{Key: "lije", Value: -1}}).SetLimit(10).SetSkip(0)
+	var err error = nil
+	var cursor *mongo.Cursor
+	cursor, err = coll.Find(context.TODO(), bson.D{}, opts)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	var results []bson.D
+	err = cursor.All(context.TODO(), &results)
+	dataArray := make([]map[string]interface{}, 0, len(results))
+	for _, result := range results {
+		dataMap := map[string]interface{}{}
+		for _, k := range result {
+			dataMap[k.Key] = k.Value
+		}
+		dataArray = append(dataArray, dataMap)
+	}
+	return dataArray, err
 }
--- Tools/mongodb/index.go
+++ Tools/mongodb/index.go
@@ -2,12 +2,9 @@ package mongodb
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
@@ -30,17 +27,4 @@ func ConnectDB(uri string) error {
 
 	mongoClient = client
 	return err
-}
-
-func GetData(client *mongo.Client) (map[string][]primitive.M, error) {
-	coll := client.Database("hvData").Collection("user")
-	var result bson.M
-	filter := bson.D{{Key: "name", Value: "최우승"}}
-	err := coll.FindOne(context.TODO(), filter).Decode(&result)
-	if err == mongo.ErrNoDocuments {
-		fmt.Printf("No document was found with the title")
-		return map[string][]primitive.M{"data": {}}, nil
-	}
-	dataArray := []primitive.M{result}
-	return map[string][]primitive.M{"data": dataArray}, err
 }
