869b8aa6cace600b2a996a4f56e71e147ffe4ce0
CWIN77 <cwstbp0426@gmail.com>
2022-12-25 00:08:35 +0900

    project get 함수 추가

--- Tools/Router/project/P/index.go
+++ Tools/Router/project/P/index.go
@@ -3,31 +3,42 @@ package _project_P
 import (
 	"context"
 	"fiber/Tools/mongodb"
-	"fmt"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 var Get = func(c *fiber.Ctx) error {
-	_, err := getData(mongodb.GetMongoClient())
+	data, err := getData(c.Params("params"))
 	if err != nil {
 		return c.Status(400).JSON(err.Error())
 	}
-	return c.SendString(c.Params("params"))
+	return c.Status(200).JSON(data)
 }
 
-func getData(client *mongo.Client) (map[string][]primitive.M, error) {
-	coll := client.Database("hvData").Collection("user")
-	var result bson.M
-	filter := bson.D{{Key: "name", Value: "최우승"}}
-	err := coll.FindOne(context.TODO(), filter).Decode(&result)
-	if err == mongo.ErrNoDocuments {
-		fmt.Printf("No document was found with the title")
-		return map[string][]primitive.M{"data": {}}, nil
+func getData(owner string) ([]map[string]interface{}, error) {
+	client := mongodb.GetMongoClient()
+	coll := client.Database("hvData").Collection("project")
+	filter := bson.M{"owner": owner}
+	opts := options.Find().SetSort(bson.M{"updatedAt": -1})
+	var err error = nil
+	var cursor *mongo.Cursor
+	cursor, err = coll.Find(context.TODO(), filter, opts)
+	if err != nil {
+		log.Fatal(err.Error())
 	}
-	dataArray := []primitive.M{result}
-	return map[string][]primitive.M{"data": dataArray}, err
+	var results []bson.D
+	err = cursor.All(context.TODO(), &results)
+	dataArray := make([]map[string]interface{}, 0, len(results))
+	for _, result := range results {
+		dataMap := map[string]interface{}{}
+		for _, k := range result {
+			dataMap[k.Key] = k.Value
+		}
+		dataArray = append(dataArray, dataMap)
+	}
+	return dataArray, err
 }
