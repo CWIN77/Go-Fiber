f1da1557d20e544247d4cb705f34cd60933a48bc
CWIN <cwstbp0426@gmail.com>
2022-12-26 11:48:32 +0900

    Update index.go

--- Tools/Router/team/P/index.go
+++ Tools/Router/team/P/index.go
@@ -8,3 +8,32 @@ var Get = func(c *fiber.Ctx) error {
 
 	return c.SendString(c.Params("params"))
 }
+
+func getData(memberId string) ([]map[string]interface{}, error) {
+        client := mongodb.GetMongoClient()
+	coll := client.Database("hvData").Collection("team")
+	filter := bson.M{"$or": [
+                bson.M{"member.master": memberId}, 
+                bson.M{"member.manager": memberId}, 
+                bson.M{"member.maker": memberId}, 
+                bson.M{"member.reader": memberId}
+	]}
+	opts := options.Find()
+	var err error = nil
+	var cursor *mongo.Cursor
+	cursor, err = coll.Find(context.TODO(), filter, opts)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	var results []bson.D
+	err = cursor.All(context.TODO(), &results)
+	dataArray := make([]map[string]interface{}, 0, len(results))
+	for _, result := range results {
+		dataMap := map[string]interface{}{}
+		for _, k := range result {
+			dataMap[k.Key] = k.Value
+		}
+		dataArray = append(dataArray, dataMap)
+	}
+	return dataArray, err
+}
