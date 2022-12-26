b198d931016c3d07f09f4d205aeb961ad7d641b3
CWIN <cwstbp0426@gmail.com>
2022-12-26 12:03:44 +0900

    Update index.go

--- Tools/Router/user/P/index.go
+++ Tools/Router/user/P/index.go
@@ -7,3 +7,13 @@ import (
 var Get = func(c *fiber.Ctx) error {
 	return c.SendString(c.Params("params"))
 }
+
+func getData(id string) (primitive.M, error) {
+ client := mongodb.GetMongoClient()
+ coll := client.Database("hvData").Collection("user")
+ var result bson.M
+ userid, _ := primitive.ObjectIDFromHex(id)
+ filter := bson.M{"_id": userid}
+ err := coll.FindOne(context.TODO(), filter).Decode(&result)
+ return result, err
+}
