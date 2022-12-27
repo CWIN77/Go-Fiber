d801f62abb4121727f704c2f4e50d9dc14ac57d8
CWIN77 <cwstbp0426@gmail.com>
2022-12-28 05:20:38 +0900

    user create 추가 완료

--- Tools/Router/user/index.go
+++ Tools/Router/user/index.go
@@ -3,12 +3,19 @@ package _user
 import (
 	"context"
 	"fiber/Tools/mongodb"
-	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 )
+
+type UserData struct {
+	UID  string `json:"uid" xml:"uid" form:"uid"`
+	NAME string `json:"name" xml:"name" form:"name"`
+	IMG  string `json:"img" xml:"img" form:"img"`
+	MAIL string `json:"mail" xml:"mail" form:"mail"`
+}
 
 var Get = func(c *fiber.Ctx) error {
 	data, err := getData(c.Params("params"))
@@ -18,20 +25,19 @@ var Get = func(c *fiber.Ctx) error {
 	return c.Status(200).JSON(data)
 }
 
-type UserData struct {
-	UID  string `json:"uid" xml:"uid" form:"uid"`
-	NAME string `json:"name" xml:"name" form:"name"`
-	IMG  string `json:"img" xml:"img" form:"img"`
-	MAIL string `json:"mail" xml:"mail" form:"mail"`
-}
-
 var Post = func(c *fiber.Ctx) error {
 	p := new(UserData)
 	if err := c.BodyParser(p); err != nil {
 		return c.Status(400).JSON(err.Error())
 	}
-	createData(p)
-	return c.Status(200).JSON([4]string{p.UID, p.NAME, p.IMG, p.MAIL})
+	if p.IMG == "" || p.MAIL == "" || p.NAME == "" || p.UID == "" {
+		return c.Status(400).JSON("Please send all user data.")
+	}
+	result, err := createData(p)
+	if err != nil {
+		return c.Status(400).JSON(err.Error())
+	}
+	return c.Status(200).JSON(result)
 }
 
 func getData(id string) (primitive.M, error) {
@@ -44,8 +50,30 @@ func getData(id string) (primitive.M, error) {
 	return result, err
 }
 
-func createData(userData *UserData) {
-	if userData.IMG != "" && userData.MAIL != "" && userData.NAME != "" && userData.UID != "" {
-		fmt.Println(userData)
+func createData(userData *UserData) (interface{}, error) {
+	client := mongodb.GetMongoClient()
+	coll := client.Database("hvData").Collection("user")
+	userId, _ := primitive.ObjectIDFromHex(userData.UID)
+	var findData bson.M
+	insertData := bson.M{
+		"name": userData.NAME,
+		"img":  userData.IMG,
+		"mail": userData.MAIL,
+		"_id":  userId,
 	}
+	if err := coll.FindOne(context.TODO(), bson.M{"_id": userId}).Decode(&findData); err == mongo.ErrNoDocuments {
+		result, err := coll.InsertOne(context.TODO(), insertData)
+		return result, err
+	} else if err != nil {
+		return nil, err
+	}
+
+	if (findData["name"] != insertData["name"]) || (findData["img"] != insertData["img"]) || (findData["mail"] != insertData["mail"]) || (findData["_id"] != insertData["_id"]) {
+		filter := bson.D{{Key: "_id", Value: userId}}
+		update := bson.M{"$set": insertData}
+		
+		updateResult, err := coll.UpdateOne(context.TODO(), filter, update)
+		return updateResult, err
+	}
+	return "Data already exist and fresh state", nil
 }
